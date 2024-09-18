c244b1b15ebaa0d8cd4aff8fc84faf77570630fe
Sharjeel <sharjeelmalik71@yahoo.com>
2024-09-18 11:57:29 -0400

    Final touchs - Trying to get to 10/10 on serve with pylint

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
+    '''Handles the API call to the AI and formats the response to only include the emotions and dominant emotion'''
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     user_input = { "raw_document": { "text": text_to_analyse } }
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+'''The application is deployed using Flask. This file contains all the routes'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +7,8 @@ app = Flask("Emotion Detectior")
 
 @app.route("/emotionDetector")
 def detector():
+    '''Takes the input from the user and send it to the API via the emotion_detector function.
+    Afterwards, it shows the emotion values of the input on the page'''
     user_input_text = request.args.get("textToAnalyze")
     response = emotion_detector(user_input_text)
 
@@ -18,11 +22,11 @@ def detector():
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
 
-    else:
-        return f"For the given statement, the system response is 'anger': {anger_val}, 'disgust': {disgust_val}, 'fear': {fear_val}, 'joy': {joy_val} and 'sadness': {sadness_val}. The dominant emotiom is {dominant_emotion}"
+    return f"For the given statement, the system response is 'anger': {anger_val}, 'disgust': {disgust_val}, 'fear': {fear_val}, 'joy': {joy_val} and 'sadness': {sadness_val}. The dominant emotiom is {dominant_emotion}"
 
 @app.route("/")
 def render_index_page():
+    ''' Renders the index page to the user'''
     return render_template("index.html")
 
 if __name__ == "__main__":
